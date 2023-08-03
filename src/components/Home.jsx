b84a6504ed51bc8c92d41316192c63a6c4ddc842
jordansaker <jordan.saker@ymail.com>
2023-08-03 18:24:43 +1000

    added sticky banner

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,18 +7,43 @@ const Home = ({ showQuote}) => {
     showQuote && (
       <>
         <h1>Home</h1>
-        <div className="container">{showQuote}</div>
-        <section className="container api">
-          <h4>API</h4>
-          <ul>
-            <li>
-              <Link to={"/api/docs/"}>Docs</Link>
-            </li>
-            <li>
-              <Link to={'https://github.com/jordansaker/dom-quotes-api'}>GitHub</Link>
-            </li>
-          </ul>
-        </section>
+          <section className="hero">
+            <p>
+              When learning about DOM Elements and how to manipulate them
+              reminds you of family.
+            </p>
+            <img
+              className="container"
+              src="https://images.pling.com/img/00/00/62/80/83/1589921/a4ba72250479192f69456e48f51cc1db0bd188a47418fcf4ad82b0e4aca4ad2f9a4f.jpg"
+              alt="torretto-family"
+            />
+          </section>
+          <div className='back'>
+          <div className="container">{showQuote}</div>
+          <section className="container d-flex api">
+            <div className="container">
+              <h4>API</h4>
+              <ul>
+                <li>
+                  <Link to={"/api/docs/"}>Docs</Link>
+                </li>
+                <li>
+                  <Link to={"https://github.com/jordansaker/dom-quotes-api"}>
+                    GitHub
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="container">
+              <h4>Disclaimer</h4>
+              <p>
+                Dom Quotes API is only intended for educational purposes on APIs
+                and client-side development. All quotable content are not owned
+                by the developer.
+              </p>
+            </div>
+          </section>
+        </div>
       </>
     )
   )
