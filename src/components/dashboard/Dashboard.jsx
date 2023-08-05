b7b39490b48dd7c6466977ff364cc34ed554addf
jordansaker <jordan.saker@ymail.com>
2023-08-05 20:00:11 +1000

    updated protected routes

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -5,7 +5,7 @@ import { auth } from '../auth/AuthModule'
 const Dashboard = ({content, nav, search}) => {
   return (
     <>
-      {auth.admin === 'Admin access' ? 
+      {auth.admin() === 'Admin access' ? 
       <div className="dashboardContent">
         <div>
           <h4>Dashboard</h4>
